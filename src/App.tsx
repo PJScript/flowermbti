e1ef929a7318532323fb91bedf54f80fcd53b940
kilo718 <kilo718@naver.com>
2021-12-17 04:33:57 +0900

    flowerPage LayOut and Design

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,30 @@ import { GlobalBody, FlowerRootdiv } from './style/global-style';
 import FlowerHome from './page/flowerMbti/flowerHome';
 import Result from './page/flowerMbti/result'
 import Flowers from './page/flowerMbti/flowers';
+import FlowerQuestion from './page/flowerMbti/flowerQuestion';
 import Home from './page/home';
 import './App.css'
+
 const App = (): JSX.Element => {
   const [minutes, setMinutes] = useState<number>(2);
   const [seconds, setSeconds] = useState<number>(59);
 
-  useEffect(() => {
-    const countdown = setInterval(() => {
-      if ((seconds) > 0) {
-        setSeconds((seconds) - 1);
-      }
-      if ((seconds) === 0) {
-        if ((minutes) === 0) {
-          clearInterval(countdown);
-        } else {
-          setMinutes((minutes) - 1);
-          setSeconds(59);
-        }
-      }
-    }, 1000);
-    return () => clearInterval(countdown);
-  }, [minutes, seconds]);
+  // useEffect(() => {
+  //   const countdown = setInterval(() => {
+  //     if ((seconds) > 0) {
+  //       setSeconds((seconds) - 1);
+  //     }
+  //     if ((seconds) === 0) {
+  //       if ((minutes) === 0) {
+  //         clearInterval(countdown);
+  //       } else {
+  //         setMinutes((minutes) - 1);
+  //         setSeconds(59);
+  //       }
+  //     }
+  //   }, 1000);
+  //   return () => clearInterval(countdown);
+  // }, [minutes, seconds]);
   return (
     <>
     <GlobalBody />
@@ -34,8 +36,9 @@ const App = (): JSX.Element => {
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/project/1" element={<FlowerHome/>}/>
-      <Route path="/flowers" element={<Flowers/>} />
-      <Route path="/result"  element={<Result/>} />
+      <Route path="/project/1/question" element={<FlowerQuestion/>} />
+      <Route path="/project/1/flowers" element={<Flowers/>} />
+      <Route path="/project/1/result"  element={<Result/>} />
     </Routes>
     </>
   );
---
+++ src/questionAndAnswer.tsx
@@ -0,0 +1,16 @@
+type QnaType = {question:string, answer:string[]}
+
+export let questionAnswer:QnaType[] = [
+  {
+    question: '질문1',
+    answer: ['대답1-1', '대답1-2', '대답1-3', '대답1-4']
+  },
+    {
+      question:'질문2',
+      answer: ['대답2-1', '대답2-2', '대답2-3', '대답2-4']
+    },
+    {
+      question:'질문3',
+      answer: ['대답3-1', '대답3-2', '대답3-3', '대답3-4']
+    }
+  ]
